3a4f7e872e66fc6096e76b350dc5f008b61a375f
lukeharley <luca.bagnato1@gmail.com>
2026-06-28 01:53:57 +0200

    Fix Compose material icons dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.animation:animation")
     implementation("androidx.compose.foundation:foundation")
-    implementation("androidx.compose.material.icons:icons-extended")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.runtime:runtime-livedata")
     implementation("androidx.compose.ui:ui")
